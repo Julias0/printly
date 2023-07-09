d5558cfaab95eeaa6945926b28014234b7683cbf
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2023-07-09 13:23:34 +0530

    Added build commands

--- build.sh
+++ build.sh
@@ -1,25 +1,4 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-STORAGE_DIR=/opt/render/project/.render
-
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
-else
-  echo "...Using Chrome from cache"
-fi
-
-sysctl -w kernel.unprivileged_userns_clone=1 
-
-# be sure to add Chromes location to the PATH as part of your Start Command
-export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
-
 # add your own build commands...
 npm install --save-dev
+
+node ./node_modules/puppeteer/install.js
